a628083767e2c2629f89ec5ab21b5a2037655217
Shriyanshi saxena <shriyanshisaxena01@gmail.com>
2025-02-27 13:21:38 +0530

    js

---
+++ eventd.js
@@ -0,0 +1,27 @@
+// let v1 = 0; // Define v1 globally
+
+// function inc() {
+//     let ct = document.getElementById('number');
+//     v1++;  // Increment the value
+//     ct.innerHTML = v1;
+// }
+
+// function dec() {
+//     let ct = document.getElementById('number');
+//     v1--;  // Decrement the value
+//     if(v1>=0)
+//     {
+//         ct.innerHTML = v1;
+//     }
+//     else{
+//         alert("not less than zero")
+//     }
+    
+// }
+
+
+
+function wung(){
+    let img = document.getElementById('immm')
+    img .src = org.jpeg
+}
